4f798fa48618f5c802f7f7c1fa3e7510611daf69
zesheng dou <douzsh@gmail.com>
2017-01-09 16:36:51 +0800

    fix bugs for some emoji not in 5 chars

--- tools.py
+++ tools.py
@@ -13,10 +13,11 @@ def emoji_dealer(name):
         for i in match:
             strOri = '<span class="emoji {0}"></span>'.format(i)
             try:
-                flag = re.search('emoji([\da-z]{5})', i).groups()
+                flag = re.search('emoji([\da-z]{5})', i).groups()[0]
             except:
                 flag = i[5:]
-            name = name.replace(strOri, ('\\U000%s'%flag).decode('unicode-escape'))
+            flag = flag.zfill(8)
+            name = name.replace(strOri, ('\\U%s'%flag).decode('unicode-escape'))
     return name
 
 
@@ -45,6 +46,8 @@ def to_unicode(string, encoding='utf-8'):
         
 if __name__ == '__main__':
     name = u'<span class="emoji emoji1f463"></span>黑白幻影<span class="emoji emoji1f49e"></span>'
+    print emoji_dealer(name)
+    name = u'ʚ<span class="emoji emoji2764"></span>ɞ'
     print emoji_dealer(name)
 
 
